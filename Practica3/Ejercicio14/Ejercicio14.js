467c200f9a19284956f7c05561e6943773eaeb11
Raul <UO265210@uniovi.es>
2021-11-30 01:35:07 +0100

    Cambios al 14

---
+++ Practica3/Ejercicio14/Ejercicio14.js
@@ -0,0 +1,23 @@
+class Ejercicio14{
+
+    pintar(){
+        var canvas = document.getElementById('canvas');
+        var canvas1 = canvas.getContext('2d');
+        canvas1.strokeStyle = "rgba(0, 0, 255, 1)";
+        canvas1.beginPath();
+        canvas1.moveTo(275, 125);
+        canvas1.quadraticCurveTo(225, 63, 300, 335);
+        canvas1.quadraticCurveTo(260, 200, 265, 200);
+        canvas1.quadraticCurveTo(99, 21, 325, 162);
+        canvas1.quadraticCurveTo(325, 125, 2, 55);
+        canvas1.closePath();
+        canvas1.stroke();
+
+    }
+
+    añadir(){
+        history.pushState({pagina: 1}, "Get sitck bugged lol", "state.html#añadir");
+    }
+}
+
+var ejer = new Ejercicio14();
